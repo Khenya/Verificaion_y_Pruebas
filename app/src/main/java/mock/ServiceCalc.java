90054bc4329cd587e16db297b464f1298733452e
Khenya <abrendakhenya@gmail.com>
2023-10-01 21:42:01 -0400

    mockito

---
+++ app/src/main/java/mock/ServiceCalc.java
@@ -0,0 +1,19 @@
+package mock;
+
+public class ServiceCalc {
+
+    public int add(int a, int b ){
+        return 0;
+    }
+    public int subs(int a, int b) {
+        return 0;
+    }
+    public int mult(int a, int b ){
+        System.out.println(" *-***** usando metodo real -********");
+        return a*b*-1;
+    }
+    public int div(int a, int b ){
+        return 0;
+    }
+
+}
